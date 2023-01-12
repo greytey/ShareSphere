37b464d4b55fe88d09aef84c703af31206f04ee8
greyforrest <melanie.voellmin@gmail.com>
2023-01-12 09:51:38 +0100

    Standart MAUI Programm

---
+++ ShareSphere/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ShareSphere;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ ShareSphere/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ShareSphere;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
