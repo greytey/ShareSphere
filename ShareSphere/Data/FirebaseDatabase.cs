6048f6daadef5fc62fa4230b7d2b8bd6498f593c
Joel Jütte <j.juette.inf20@stud.bbbaden.ch>
2023-02-02 11:03:13 +0100

    updated CRUD

--- ShareSphere/Data/FirebaseDatabase.cs
+++ ShareSphere/Data/FirebaseDatabase.cs
@@ -17,29 +17,64 @@ namespace ShareSphere.Data
         public async Task addGamer(Gamer gamer)
         {
             await firebaseClient
-              .Child("gamers") 
+              .Child("gamers")
+              .Child(gamer.userId)
               .PostAsync(gamer);
         }
 
-        public async Task<List<KeyValuePair<string, Gamer>>> getGamers()
+        public async Task<List<Gamer>> getGamers()
         {
             var gamers = await firebaseClient
               .Child("gamers")
               .OnceAsync<Gamer>();
 
-            return gamers?
-              .Select(x => new KeyValuePair<string, Gamer>(x.Key, x.Object))
-              .ToList();
+            return gamers?.Select(item => new Gamer
+            {
+                userId = item.Object.userId,
+                username = item.Object.username,
+                biography = item.Object.biography,
+                platforms = item.Object.platforms,
+                games = item.Object.games,
+                joinedAsString = item.Object.joinedAsString,
+            }).ToList();
         }
 
         public async void updateGamer(string id, Gamer gamer)
         {
-            await firebaseClient.Child("gamers").Child(id).PutAsync(gamer);
+            await firebaseClient.Child("gamers").Child(gamer.userId).Child(id).PutAsync(gamer);
         }
 
-        public async void removeGamer(string id)
+        public async void removeGamer(Gamer gamer)
         {
-            await firebaseClient.Child("gamers").Child(id).DeleteAsync();
+            await firebaseClient.Child("gamers").Child(gamer.userId).DeleteAsync();
+        }
+
+        public async Task<Gamer> getGamerByUid(string uid)
+        {
+            List<Gamer> gamers = await getGamers();
+
+            foreach(Gamer gamer in gamers)
+            {
+                if (gamer.userId.Equals(uid))
+                {
+                    return gamer;
+                }
+            }
+            return null;
+        }
+
+        public async Task<Gamer> getUsername(string username)
+        {
+            List<Gamer> gamers = await getGamers();
+
+            foreach (Gamer gamer in gamers)
+            {
+                if (gamer.userId.Contains(username))
+                {
+                    return gamer;
+                }
+            }
+            return null;
         }
     }
 }
